5eba4495cf71ab1b6413a5a9ec36153ce20b0890
Leo Espindola <leo0.moreirah@gmail.com>
2025-06-16 12:37:23 -0300

    get image

--- src/components/ImgInput.tsx
+++ src/components/ImgInput.tsx
@@ -1,33 +1,40 @@
-import React from 'react';
+import { useState } from "react"
+import CropImg from "../features/analyzer/CropImg"
 
-interface ButtonProps {
-  onClick?: () => void;
-}
+const ImgInput = () => {
+  const [imageSrc, setImageSrc] = useState<string | null>(null)
 
-const ImgInput: React.FC<ButtonProps> = ({ onClick }) => {
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (file) {
+      const url = URL.createObjectURL(file)
+      setImageSrc(url)
+    }
+  }
+
   return (
-    <div className='flex flex-col items-center'>
-      <div className='relative inline-block'>
-        <div onClick={onClick} className='
-          p-2
-          bg-linear-to-t from-sky-500 to-indigo-500 cursor-pointer 
-          hover:bg-linear-to-bl hover:from-violet-500
-          rounded-full shadow-lg'
-        >
-          <img src="./src/assets/photo_camera.svg" alt="Enviar Imagem"/>
-          <input type="file" accept='image/*' className="
-            absolute 
-            top-0 
-            left-0 
-            w-full 
-            h-full 
-            opacity-0 
-            cursor-pointer
-            rounded-full"/>
+    <>
+      {imageSrc &&  <CropImg imageSrc={imageSrc} onCancel={() => setImageSrc(null)} />}
+      <div className='flex flex-col items-center'>
+        <div className='relative inline-block'>
+          <div className='
+            p-2
+            bg-gradient-to-t from-sky-500 to-indigo-500 cursor-pointer 
+            hover:bg-gradient-to-bl hover:from-violet-500
+            rounded-full shadow-lg'
+          >
+            <img src="./src/assets/photo_camera.svg" alt="Enviar Imagem" />
+            <input
+              type="file"
+              accept='image/*'
+              className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer rounded-full"
+              onChange={handleFileChange}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default ImgInput;
+export default ImgInput
